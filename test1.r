be8438ebcf7a46d4fc519bf686bbe6ca7bedc16c
mj <whalsrb100@gmail.com>
2020-08-28 10:00:02 +0900

    auto commit

--- test1.r
+++ test1.r
@@ -1,4 +1,6 @@
 library(rvest)
+library(jsonlite)
+library(httr)
 
 worldkorea_login <- 'https://mticket.lotteworld.com/web/main/loginView.do?reUrl=https%3A%2F%2Fmticket.lotteworld.com%2Fweb%2Fpay%2FpayManageInfo.do%3Fei_gsber%3D1210%26ei_bukrs%3D1200&ei_gsber=1210&ei_bukrs=1200'
 
@@ -6,4 +8,4 @@ worldkorea_login <- 'https://mticket.lotteworld.com/web/main/loginView.do?reUrl=
 lotto_base <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
 lotto_num <- '900'
 lotto_url <- paste(lotto_base, lotto_num, sep='')
-read_html(lotto_url)
+read_html(lotto_url[1])
