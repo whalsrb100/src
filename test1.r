562e73befb0877ea2568db68b99078b9a2851642
mj <whalsrb100@gmail.com>
2020-08-28 10:07:02 +0900

    auto commit

--- test1.r
+++ test1.r
@@ -6,7 +6,7 @@ library(dplyr)
 worldkorea_login <- 'https://mticket.lotteworld.com/web/main/loginView.do?reUrl=https%3A%2F%2Fmticket.lotteworld.com%2Fweb%2Fpay%2FpayManageInfo.do%3Fei_gsber%3D1210%26ei_bukrs%3D1200&ei_gsber=1210&ei_bukrs=1200'
 
 
-lotto_base <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
-lotto_num <- '900'
-lotto_url <- paste(lotto_base, lotto_num, sep='')
-Result <- read_html(lotto_url[1])
+base <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
+num <- '900'
+url <- paste(base, num, sep='')
+urltohtml <- read_html(url[1])
