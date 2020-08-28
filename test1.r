c6045d9c1045309b3de15b79509e0da7ba431c84
mj <whalsrb100@gmail.com>
2020-08-28 13:00:02 +0900

    auto commit

--- test1.r
+++ test1.r
@@ -4,9 +4,8 @@ library(httr)
 library(dplyr)
 
 worldkorea_login <- 'https://mticket.lotteworld.com/web/main/loginView.do?reUrl=https%3A%2F%2Fmticket.lotteworld.com%2Fweb%2Fpay%2FpayManageInfo.do%3Fei_gsber%3D1210%26ei_bukrs%3D1200&ei_gsber=1210&ei_bukrs=1200'
-
-
 base <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
 num <- '900'
 url <- paste(base, num, sep='')
-urltohtml <- read_html(url[1])
+urltohtml <- as.character(read_html(url[1]))
+rest = fromJSON(strsplit(strsplit(urltohtml,split='<p>')[[1]][2],split="</p>")[[1]][1])
