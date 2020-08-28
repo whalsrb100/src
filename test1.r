b4a60dead5dcc19ee5de1beb3f92a0576ffa5b45
mj <whalsrb100@gmail.com>
2020-08-28 10:02:02 +0900

    auto commit

--- test1.r
+++ test1.r
@@ -1,6 +1,7 @@
 library(rvest)
 library(jsonlite)
 library(httr)
+library(dplyr)
 
 worldkorea_login <- 'https://mticket.lotteworld.com/web/main/loginView.do?reUrl=https%3A%2F%2Fmticket.lotteworld.com%2Fweb%2Fpay%2FpayManageInfo.do%3Fei_gsber%3D1210%26ei_bukrs%3D1200&ei_gsber=1210&ei_bukrs=1200'
 
