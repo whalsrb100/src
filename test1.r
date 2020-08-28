0649e183850777d92b0d2f247a22563f77bb3d95
mj <whalsrb100@gmail.com>
2020-08-28 09:54:26 +0900

    auto commit

--- test1.r
+++ test1.r
@@ -1,14 +1,7 @@
-print("Hello, test000__01")
-print("Hello~~")
-print("asdf222")
-print("test444")
-print("test555")
-my_url1 <- 'https://mticket.lotteworld.com/web/main/loginView.do?reUrl=https%3A%2F%2Fmticket.lotteworld.com%2Fweb%2Fpay%2FpayManageInfo.do%3Fei_gsber%3D1210%26ei_bukrs%3D1200&ei_gsber=1210&ei_bukrs=1200'
+worldkorea_login <- 'https://mticket.lotteworld.com/web/main/loginView.do?reUrl=https%3A%2F%2Fmticket.lotteworld.com%2Fweb%2Fpay%2FpayManageInfo.do%3Fei_gsber%3D1210%26ei_bukrs%3D1200&ei_gsber=1210&ei_bukrs=1200'
 
 
 lotto_addr <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
 num <- '900'
 lotto_url <- paste(lotto_addr, num, sep='')
-
 print(lotto_url)
-print("1234")
